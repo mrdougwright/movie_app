5346f29614d23092e4ff354d15a7076fac1a30c9
Slava Zagorodnii <kr3ved@gmail.com>
2018-05-10 20:46:01 -0700

    very secure. much wow

--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -1,2 +1,7 @@
 class MovieController < ApplicationController
+  def index
+    return render unless params[:y]
+    movie = JSON.parse(Curl.get("http://www.omdbapi.com/?y=#{params[:y]}&t=a&apikey=37bece07").body_str)
+    @password = movie.slice("Title", "Genre", "Actors").values.flatten.shuffle(random: Random.new(params[:y].to_i)).join(" ").split(" ").take(5).join(" ").parameterize
+  end
 end
